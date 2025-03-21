301e6a7da5ff820fd9543fd02ab392da63dafeb6
Polute <ldemaria2003@gmail.com>
2025-03-21 18:26:01 +0100

    Api login y register completada, a la espera de la BD

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from fastapi import APIRouter
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
-# http://127.0.0.1:8000/login Puerto FRONT
-# http://127.0.0.1:8000/rlogin  Puerto BACK
+# http://127.0.0.1:8000/api/login Puerto FRONT
+# http://127.0.0.1:8000/api/rlogin  Puerto BACK
 # http://127.0.0.1:8000/register  Puerto FRONT
 
 app = FastAPI()
 
 from user import router1
 from user import router2
+from user import router3
+from user import router4
 
 app.include_router(router1)
 app.include_router(router2)
-
+app.include_router(router3)
+app.include_router(router4)
--- user.py
+++ user.py
@@ -14,6 +14,9 @@ import json
 class user_psw(BaseModel): 
     correo: str
     contraseña: str
+    
+class user_id(BaseModel): 
+    usr_id: str
 
 class register_user(BaseModel):
     correo: str
@@ -22,22 +25,55 @@ class register_user(BaseModel):
     DNI: str
     tipo: str
 
+class register_state(BaseModel):
+    ok: bool
+
 router1 = APIRouter()
 router2 = APIRouter()
+router3 = APIRouter()
+router4 = APIRouter()
 
 
-
-@router1.get("/api/login")
-async def enviar_datosH(login_data: user_psw, url: str):
+@router1.post("/api/login")
+async def login(login_data: user_psw):
     # Convertir los datos del modelo Pydantic a un diccionario
     login_dict = login_data.dict()
+    url = "http://127.0.0.1:8000/api/rlogin"
+
 
     # Enviar los datos al endpoint especificado
     async with httpx.AsyncClient() as client: #De esta forma, se envian los datos simultaneamente mientras espero una respuesta
         response = await client.post(url, json=login_dict)
 
     # Devolver la respuesta del servidor(Se decodifica y se devuelve en formato json)
-    return json.loads(response.content.decode())
+    return response.json()
+
+@router2.post("/api/rlogin")
+async def rlogin(usuario_data: user_psw):  # Recibe un diccionario con los datos del usuario
+    print("Sesión iniciada correctamente")
+    user_id = {"usr_id": "12345"}  # Devuelve un diccionario válido
+    return user_id
 
 
+@router3.post("/api/register")
+async def register(register_data: register_user):
+    register_dict = register_data.dict()
+    url = "http://127.0.0.1:8000/api/okregister"
+
+
+    # Enviar los datos al endpoint especificado
+    async with httpx.AsyncClient() as client: #De esta forma, se envian los datos simultaneamente mientras espero una respuesta
+        response = await client.post(url, json=register_dict)
+
+
+    if (response.json()):
+        return "Registro completado"
+    else: 
+        return "Registro inválido"
+
+@router4.post("/api/registerok")
+async def rlogin(register_data: register_user):  # Recibe un diccionario con los datos del usuario
+    print("Registro completado correctamente")
+    ok = True
+    return ok
 
