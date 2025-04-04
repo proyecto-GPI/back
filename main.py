f76c1a69a128891262280267433480981fbd5fc3
ALBERTO GARCIA GARCIA <alberto.garciaga@alumnos.upm.es>
2025-04-04 14:29:06 +0200

    Añadido routers en main.py

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 # http://127.0.0.1:8000/api/login Puerto FRONT
-# http://127.0.0.1:8000/api/rlogin  Puerto BACK
-# http://127.0.0.1:8000/register  Puerto FRONT
-
+# http://127.0.0.1:8000/api/rlogin Puerto BACK
+# http://127.0.0.1:8000/api/register Puerto FRONT
+# http://127.0.0.1:8000/api/okregister Puerto FRONT
 # http://127.0.0.1:8000/api/oficinas Puerto FRONT
+# http://127.0.0.1:8000/api/availability Puerto FRONT
+# http://127.0.0.1:8000/api/{id_user}/reserve Puerto FRONT
+# http://127.0.0.1:8000/api/{id_user}/rreserve Puerto BACK
+
 
 app = FastAPI()
 
@@ -18,17 +22,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-from user import router1
-from user import router2
-from user import router3
-from user import router4
+from user import router1, router2, router3, router4
 from offices import router5
 from availability import router6
+from reservas import router7, router8
 
 app.include_router(router1)
 app.include_router(router2)
 app.include_router(router3)
 app.include_router(router4)
+
 app.include_router(router5)
+
 app.include_router(router6)
+
+app.include_router(router7)
+app.include_router(router8)
 
